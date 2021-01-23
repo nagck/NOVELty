c0ea1f505ee9c455dfe25c8fb93a12a81581e73d
NageshK <nagck@hotmail.com>
2021-01-23 14:44:52 -0500

    Updated models with schema/table creation scripts

---
+++ models/books.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+    const Books = sequelize.define('Books', {
+    //   bookID: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: false,
+    //     primaryKey: true,
+    //     autoIncrement: true
+    //   },  
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 240],
+        },
+      },
+      ISBN: {
+        type: DataTypes.STRING,
+        //allowNull: false,
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 140],
+        }
+      },
+      URL: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+    });
+  
+    Books.associate = (models) => {
+      // Associating Books with Review
+      // When a Book is deleted, also delete any associated Review
+      models.Books.hasMany(models.Review, models.Reading, {
+        onDelete: 'cascade',
+        //foreignKey: 'userID'
+      });
+    };
+  
+    return Books;
+  };
+  
