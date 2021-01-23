3b4b21916cf650976447f072f8a30f7daaeacb0e
Hector Garcia Millares <hgarcia.millares@gmail.com>
2021-01-22 21:08:18 -0500

    Adding initial HTML code to test UI base. Adding /Views/Layouts/Partials and /images directory folders and related files

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 
 // Requiring our routes
 
