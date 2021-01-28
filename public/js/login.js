c1a6b1b1669b4b563d8943d14cce86c3846b4335
Annie Kwan <akwan.mtl@gmail.com>
2021-01-27 19:33:04 -0500

    reformatted .js files
    added error msg when login or signup failed
    
    found small typo in index.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+$(document).ready(() => {
     // Getting references to our form and inputs
-    var loginForm = $("form.login");
-    var emailInput = $("input#email-input");
-    var passwordInput = $("input#password-input");
+    const loginForm = $("form.login");
+    const emailInput = $("input#email-input");
+    const passwordInput = $("input#password-input");
   
     // When the form is submitted, we validate there's an email and password entered
-    loginForm.on("submit", function(event) {
+    loginForm.on("submit", (event) => {
       event.preventDefault();
-      var userData = {
+      const userData = {
         email: emailInput.val().trim(),
         password: passwordInput.val().trim()
       };
@@ -23,18 +23,22 @@ $(document).ready(function() {
     });
   
     // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-    function loginUser(email, password) {
+    const loginUser = (email, password) => {
       $.post("/api/login", {
         email: email,
         password: password
       })
-        .then(function() {
+        .then(() => {
           window.location.replace("/index");
           // If there's an error, log the error
         })
-        .catch(function(err) {
-          console.log(err);
-        });
+        .catch(handleLoginErr);
+    }
+
+    const handleLoginErr = (err) => {
+      console.log(err)
+      $("#alert .msg").text('Invalid Username or Password');
+      $("#alert").fadeIn(500);
     }
   });
   
